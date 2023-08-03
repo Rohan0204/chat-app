40dbe0a33fb973980aff47b882c64b0d40413b80
Rohan <rohan.dubey558@gmail.com>
2023-08-03 19:40:48 +0530

    Fresh changes

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server listening to port ${port}`);
+})
